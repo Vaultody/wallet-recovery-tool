6b7a1371fe22168bfd7ab20a9da8e3c526ac1529
Malin Ivanov <malin.ivanov@globalinfra.tech>
2025-11-26 09:54:45 +0200

    #noticket: apply context only on mac

--- src/base.js
+++ src/base.js
@@ -23,26 +23,28 @@ class Base {
             app.quit();
         });
 
-        mainWindow.webContents.on('context-menu', (e, props) => {
-            const { selectionText, isEditable } = props;
-            if (isEditable || selectionText.length > 0) {
-                const menuTemplate = [];
-                if (isEditable) {
-                    menuTemplate.push({ role: 'undo' });
-                    menuTemplate.push({ role: 'redo' });
-                    menuTemplate.push({ type: 'separator' });
-                    menuTemplate.push({ role: 'cut' });
+        if (IS_MAC) {
+            mainWindow.webContents.on('context-menu', (e, props) => {
+                const {selectionText, isEditable} = props;
+                if (isEditable || selectionText.length > 0) {
+                    const menuTemplate = [];
+                    if (isEditable) {
+                        menuTemplate.push({role: 'undo'});
+                        menuTemplate.push({role: 'redo'});
+                        menuTemplate.push({type: 'separator'});
+                        menuTemplate.push({role: 'cut'});
+                    }
+                    menuTemplate.push({role: 'copy'});
+                    if (isEditable) {
+                        menuTemplate.push({role: 'paste'});
+                        menuTemplate.push({role: 'delete'});
+                        menuTemplate.push({type: 'separator'});
+                        menuTemplate.push({role: 'selectAll'});
+                    }
+                    Menu.buildFromTemplate(menuTemplate).popup(mainWindow);
                 }
-                menuTemplate.push({ role: 'copy' });
-                if (isEditable) {
-                    menuTemplate.push({ role: 'paste' });
-                    menuTemplate.push({ role: 'delete' });
-                    menuTemplate.push({ type: 'separator' });
-                    menuTemplate.push({ role: 'selectAll' });
-                }
-                Menu.buildFromTemplate(menuTemplate).popup(mainWindow);
-            }
-        });
+            });
+        }
 
         return mainWindow;
     }
